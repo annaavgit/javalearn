cca06f51c59c7d73fd009dc9a4e213815d616b19
mr.Prog <mister_prog@mail.ru>
2020-02-05 17:00:23 +0300

    First Java lesson (EasyUm)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,23 @@
-package com.company;
+package com.company; // базовый namespace - лучше задавать доменном-компании-наоборот
+
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello world!");
         // write your code here
+        int a = 5;
+        int b = 7;
+        int c = 2, d = 3;
+        System.out.println("Переменная а равна " + a + "\nПеременная b равна " + b
+                + "\nСумма a и b равна " + (a + b));
+
+        System.out.println("Теперь выводим возвращаемое значение метода sum: " + test(20,30));
+
+    }
+
+    static int test (int a, int b)
+    {
+        return a + b;
     }
 }
